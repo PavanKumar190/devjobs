6183dde25b2d5a91919225fd53c7b3127f64dae9
PavanKumar190 <chinnapolupavankumar@gmail.com>
2023-07-17 17:45:11 +0530

    first commit

--- js/main.js
+++ js/main.js
@@ -40,3 +40,90 @@
 		responsive: true
 	});
 })(jQuery);
+
+// Start of Tawk.to Script-->
+
+var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+(function(){
+var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+s1.async=true;
+s1.src='https://embed.tawk.to/64b3c83ecc26a871b028b35a/1h5f4cdre';
+s1.charset='UTF-8';
+s1.setAttribute('crossorigin','*');
+s0.parentNode.insertBefore(s1,s0);
+})();
+
+// End of Tawk.to Script-->
+
+
+  function validation(){
+
+    var name = $("#fullName").val();
+    var email = $("#email").val();
+    var message = $("#message").val();
+    var phone = $("#phone").val();
+
+    if (name === "" || email === "" || message === "" || phone === ""){
+      swal({
+        title: "Fields Empty!!",
+        text: "Please check the missing field!!",
+  
+        button: "Ok!",
+      });
+    }
+    else{
+      swal({
+        title: "SuccessFully Submitted",
+        button: "Yes!!",
+      }); 
+    } 
+  }   
+  
+
+
+  var form = document.getElementById("sheetdb-form");
+  form.addEventListener("submit", e => {
+    e.preventDefault();
+    fetch(form.action, {
+        method : "POST",
+        body: new FormData(document.getElementById("sheetdb-form")),
+    }).then(
+        response => response.json()
+    ).then((html) => {
+      // you can put any JS code here
+      
+    });
+  });
+
+  
+  var form = document.getElementById("subform");
+  form.addEventListener("submit", e => {
+    e.preventDefault();
+    fetch(form.action, {
+        method : "POST",
+        body: new FormData(document.getElementById("subform")),
+    }).then(
+        response => response.json()
+    ).then((html) => {
+      // you can put any JS code here
+      
+    });
+  });
+
+
+  
+     let url = 'https://api.sheety.co/fd0f3ef445e9ba4c373d5accfec82ada/subscription/sheet1';
+  let body = {
+    sheet1: {
+      email
+    }
+  }
+  fetch(url, {
+    method: 'POST',
+    body: JSON.stringify(body)
+  })
+  .then((response) => response.json())
+  .then(json => {
+    
+  });
+  
